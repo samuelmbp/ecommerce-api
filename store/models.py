636749a391cd20663cf1998bb59f13d41c2be99d
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-01 12:19:23 +0100

    Create order model class for the store app and choice fields

--- store/models.py
+++ store/models.py
@@ -1,3 +1,5 @@
+from random import choices
+from wsgiref.simple_server import demo_app
 from django.db import models
 
 
@@ -10,8 +12,36 @@ class Product(models.Model):
 
 
 class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=255)
     birth_date = models.DateField(null=True)
+    membership = models.CharField(
+        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+
+
+class Order(models.Model):
+    PENDING_PAYMENT_STATUS = 'P'
+    COMPLETE_PAYMENT_STATUS = 'C'
+    FAILED_PAYMENT_STATUS = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PENDING_PAYMENT_STATUS, 'Pending'),
+        (COMPLETE_PAYMENT_STATUS, 'Complete'),
+        (FAILED_PAYMENT_STATUS, 'Failed'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PENDING_PAYMENT_STATUS)
