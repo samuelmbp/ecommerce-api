ddbaf72f01290d9a38ec707e37262d1c58dd6617
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-14 12:00:02 +0100

    Django ORM: retrieving objects

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from store.models import Product
 
-# Create your views here.
+# NOTE: Every models class has a manager 'objects'
 
 
 def say_hello(request):
+    exists = Product.objects.filter(pk=0).exists()
+
     return render(request, 'hello.html', {'name': 'Samuel'})
