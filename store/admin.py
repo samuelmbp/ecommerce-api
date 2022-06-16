f093598527fda1448bcab5aea2095ad5ca5bc1bd
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-16 18:50:56 +0100

    Add inventory status column to the product model in the admin site

--- store/admin.py
+++ store/admin.py
@@ -1,19 +1,28 @@
 from django.contrib import admin
 from . import models
 
+
 @admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin): # Convention: ModelClassAdmin
+class ProductAdmin(admin.ModelAdmin):  # Convention: ModelClassAdmin
     # Attributes
-    list_display = ['title', 'unit_price']
+    list_display = ['title', 'unit_price', 'inventory_status']
     list_editable = ['unit_price']
     list_per_page = 10
 
+    @admin.display(ordering='inventory')
+    def inventory_status(self, product):  # Computed Columns
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
+
+
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-  list_display = ['first_name', 'last_name', 'membership']
-  list_editable = ['membership']
-  ordering = ['first_name', 'last_name']
-  list_per_page = 10
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+
 
 # Register store models
 admin.site.register(models.Collection)
