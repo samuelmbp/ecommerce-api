3fb1907cc6ee8f269c85ff35392cdcc9a8b16dab
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-16 19:09:34 +0100

    Select related objects - order and customer models

--- store/admin.py
+++ store/admin.py
@@ -5,9 +5,14 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):  # Convention: ModelClassAdmin
     # Attributes
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price',
+                    'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
 
     @admin.display(ordering='inventory')
     def inventory_status(self, product):  # Computed Columns
@@ -22,6 +27,11 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
 
 
 # Register store models
--- store/models.py
+++ store/models.py
@@ -57,11 +57,11 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
-    # def __str__(self):
-    #     return self.first_name
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
-    # class Meta:
-    #     ordering = ['first_name']
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 class Order(models.Model):
     PENDING_PAYMENT_STATUS = 'P'
