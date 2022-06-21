035009c7c15de965188e300953ac8ba4ddda59f2
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-21 11:37:00 +0100

    Can delete a product object

--- store/models.py
+++ store/models.py
@@ -87,7 +87,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orderitems')
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from os import stat
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
@@ -25,7 +26,7 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def product_detail(request, id):
     # product = Product.objects.get(pk=id)
     product = get_object_or_404(Product, pk=id)
@@ -37,6 +38,14 @@ def product_detail(request, id):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response(
+                {'error': 'Product cannot be deleted as it is associated with an order item.'},
+                status=status.HTTP_405_METHOD_NOT_ALLOWED
+            )
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
