be0eb8375fe9b0d4883871228231198a9547365f
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-21 11:21:41 +0100

    Can save and update a product when created

--- store/serializers.py
+++ store/serializers.py
@@ -14,7 +14,8 @@ class ProductSerializer(serializers.ModelSerializer):
     # ModelSerializer -> rest-framework(avoids fields repetition)
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection']
 
     price_with_tax = serializers.SerializerMethodField(
         method_name='calculate_tax')
