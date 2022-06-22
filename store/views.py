d0c566b72b79e471f44576134ab11c0ad6cd6804
Samuel Raducan <samuel.thembp@gmail.com>
2022-06-22 12:23:38 +0100

    Rrefactor and clean the code in the CollectionList view by using mixins

--- store/views.py
+++ store/views.py
@@ -50,18 +50,10 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
-        queryset = Collection.objects.annotate(
-            products_count=Count('products')).all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
