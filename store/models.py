715238e51660921396e40a885e024c1b3d7944b5
Samuel Raducan <samuel.thembp@gmail.com>
2022-07-12 08:09:36 +0100

    Add validation when uploading files/images

--- store/models.py
+++ store/models.py
@@ -4,6 +4,8 @@ from django.conf import settings
 from django.core.validators import MinValueValidator
 from uuid import uuid4
 
+from .validators import validate_file_size
+
 
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
@@ -49,7 +51,10 @@ class Product(models.Model):
 class ProductImage(models.Model):
     product = models.ForeignKey(
         Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(
+        upload_to='store/images',
+        validators=[validate_file_size]
+    )
 
 
 class Customer(models.Model):
